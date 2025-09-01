eb12ea2ac0bc9e351aa5a42aa68d1da6b3eff771
abhishek-kumar1909 <abhishek.growth003@gmail.com>
2025-09-01 19:02:00 +0530

    New Code

---
+++ arthimatic_operator.py
@@ -0,0 +1,22 @@
+ #print Sum
+a = 2
+b = 5
+sum = (a+b)
+print(sum)
+
+#print diff
+c = 1000
+d = 500
+diff = c - d
+print(diff)
+
+# Arithimatic operators
+e = 8
+f = 3
+
+print(e+f)
+print(e-f)
+print(e*f)
+print(e/f)
+print(e%f) # modulo operator to find out the remainder.
+print(e**f) #power operator e^f 'e to the power f'
