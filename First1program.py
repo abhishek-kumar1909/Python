a381c7bdb6715e6f972292562d2d35c6bb6c31a9
abhishek-kumar1909 <abhishek.growth003@gmail.com>
2025-09-02 02:20:10 +0530

    added sub Repo

---
+++ First1program.py
@@ -0,0 +1,9 @@
+print("My name is Abhishek Kumar") # to print string 
+print("My age is 34")
+print("My name is Abhishek Kumar.", "My age is 34") # to print multiple string in one line
+print(34) #to print mumber
+print(25)
+print(45+5) # to print calculation
+print(25*2)
+print(4/2)
+print(8%2)
---
+++ First2prog.py
@@ -0,0 +1,23 @@
+name = "Abhishek" # string
+age = 23 # integir
+price = 101.99 #floating number
+age2 = age
+
+print("name") #print the string in ""
+print(name) #print the value of variable
+print(age)
+print(price)
+print("My name is :", name) # print the value of variable with sentence.
+print("My age is :", age)
+print("My price is :", price)
+print("His age is :", age2) # age is assigned as a value of varaiable age2, which will print the value of age which is assigned to it.
+
+old = False
+a = None
+
+print(type(name)) #print the type/class of variable as per value store in it
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
